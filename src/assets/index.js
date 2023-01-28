86be3e3354e8d5ce1c4dc89e58a8865cd6b68876
shubhkhanna <khannashubh2001@gmail.com>
2023-01-29 02:33:27 +0530

    feat: common export for assets

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import WebLogo from "./icons/web_logo.svg";
+import MobileLogo from "./icons/mobile_logo.svg";
+import HeroCoding from "./icons/hero_coding.svg";
+import HeroHtml from "./icons/hero_html.svg";
+import HeroJs from "./icons/hero_js.svg";
+import HeroPaintbrush from "./icons/hero_paintbrush.svg";
+import HeroCodetag from "./icons/hero_codetag.svg";
+import HeroDino from "./icons/hero_dino.svg";
+import HeroFlower from "./icons/hero_flower.svg";
+
+export const Icons = {
+  web_logo: WebLogo,
+  mobile_logo: MobileLogo,
+  hero_coding: HeroCoding,
+  hero_html: HeroHtml,
+  hero_js: HeroJs,
+  hero_paintbrush: HeroPaintbrush,
+  hero_codetag: HeroCodetag,
+  hero_dino: HeroDino,
+  hero_flower: HeroFlower,
+};
