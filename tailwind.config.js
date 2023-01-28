7d88f0941e8abbd16e680110193b67f3fd910a0e
shubhkhanna <khannashubh2001@gmail.com>
2023-01-29 02:32:10 +0530

    feat: extending tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Be Vietnam Pro", "Inter", "system-ui", "sans"],
+      monospace: ["DM Mono", "monospace"],
+    },
+    extend: {
+      backgroundColor: {
+        darkshade1: "#000a1e",
+      },
+      rotate: {
+        360: "360deg",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeInAndBounce: "fadeIn 3s ease-out",
+      },
+      willChange: {
+        projectCard: "border-color, opacity, transform",
+      },
+    },
   },
   plugins: [],
 };
