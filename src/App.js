fc2d23d0031c6ec96a7ff16d154119c4799da0ec
shubhkhanna <khannashubh2001@gmail.com>
2023-01-30 00:15:36 +0530

    feat: main root js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { ROUTES } from "./utils/routes";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import PageWrapper from "./components/common/PageWrapper";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <PageWrapper>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.PROJECTS} element={<ProjectsPage />} />
+      </Routes>
+    </PageWrapper>
+  );
 };
 
 export default App;
