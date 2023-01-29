dcd4f1a3a774449eb6ae48ef82c97f18aff689cd
shubhkhanna <khannashubh2001@gmail.com>
2023-01-30 00:10:43 +0530

    feat: added new icons for navbar

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
-import WebLogo from "./icons/web_logo.svg";
-import MobileLogo from "./icons/mobile_logo.svg";
+import WebNavLogo from "./icons/webNav_logo.svg";
+import MobileNavLogo from "./icons/mobileNav_logo.svg";
 import HeroCoding from "./icons/hero_coding.svg";
 import HeroHtml from "./icons/hero_html.svg";
 import HeroJs from "./icons/hero_js.svg";
@@ -9,8 +9,8 @@ import HeroDino from "./icons/hero_dino.svg";
 import HeroFlower from "./icons/hero_flower.svg";
 
 export const Icons = {
-  web_logo: WebLogo,
-  mobile_logo: MobileLogo,
+  webNav_logo: WebNavLogo,
+  mobileNav_logo: MobileNavLogo,
   hero_coding: HeroCoding,
   hero_html: HeroHtml,
   hero_js: HeroJs,
