feb6ccab889a83ef023542f55d991bbc8efe4791
shubhkhanna <khannashubh2001@gmail.com>
2023-02-06 16:03:50 +0530

    feat: added skills section icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import WebNavLogo from "./icons/webNav_logo.svg";
 import MobileNavLogo from "./icons/mobileNav_logo.svg";
+
+// Hero Section icons
 import HeroCoding from "./icons/hero_coding.svg";
 import HeroHtml from "./icons/hero_html.svg";
 import HeroJs from "./icons/hero_js.svg";
@@ -8,9 +10,18 @@ import HeroCodetag from "./icons/hero_codetag.svg";
 import HeroDino from "./icons/hero_dino.svg";
 import HeroFlower from "./icons/hero_flower.svg";
 
+// Skills icons
+import SkillsLaptop from "./icons/skills_laptop.svg";
+import SkillsCodetag from "./icons/skills_codetag.svg";
+import SkillsYoutube from "./icons/skills_youtube.svg";
+import SkillsFillStar from "./icons/skills_fillStar.svg";
+import SkillsStarOutline from "./icons/skills_starOutline.svg";
+
 export const Icons = {
   webNav_logo: WebNavLogo,
   mobileNav_logo: MobileNavLogo,
+
+  // hero section icons
   hero_coding: HeroCoding,
   hero_html: HeroHtml,
   hero_js: HeroJs,
@@ -18,4 +29,11 @@ export const Icons = {
   hero_codetag: HeroCodetag,
   hero_dino: HeroDino,
   hero_flower: HeroFlower,
+
+  // skills icons
+  skills_laptop: SkillsLaptop,
+  skills_codetag: SkillsCodetag,
+  skills_youtube: SkillsYoutube,
+  skills_fillStar: SkillsFillStar,
+  skills_starOutline: SkillsStarOutline,
 };
