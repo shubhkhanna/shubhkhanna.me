b48e074baf0470caff5350555286bebb0c17681d
shubhkhanna <khannashubh2001@gmail.com>
2023-02-08 17:16:59 +0530

    chore: project page import

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const ProjectsPage = () => {
-  return <div>ProjectsPage</div>;
+  return (
+    <>
+      <Helmet>
+        <title>Projects - shubhkhanna.in</title>
+        <meta
+          name="description"
+          content="Here are some of my side projects. I love coding and building stuff with tools like React, React Native, Node.js, Express, MongoDB, and more."
+        />
+        <link rel="canonical" href="/projects" />
+      </Helmet>
+    </>
+  );
 };
 
 export default ProjectsPage;
