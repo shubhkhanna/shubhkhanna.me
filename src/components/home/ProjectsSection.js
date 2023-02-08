a39c40100b4b5c8004fc1c634d311ebf36b62d58
shubhkhanna <khannashubh2001@gmail.com>
2023-02-08 17:15:56 +0530

    feat: project section

---
+++ src/components/home/ProjectsSection.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import SectionTitle from "../common/SectionTitle";
+
+const ProjectsSection = () => {
+  return (
+    <div className="flex flex-col text-left justify-between pt-10 relative">
+      <div id="projects">
+        <SectionTitle title="Here are a few of my favorite projects." />
+      </div>
+
+      <div className="relative w-full mt-2">
+        <Link to="/projects">
+          <div className="mt-8 max-w-sm md:max-w-2xl border border-sky-500 mx-auto text-center w-full whitespace-nowrap px-8 py-3 rounded-full text-sky-500 bg-darkshade1 hover:bg-sky-500 hover:text-white transition-colors cursor-pointer">
+            View All
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsSection;
